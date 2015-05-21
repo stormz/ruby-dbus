f4ec3ea0c3de0d39933ba06d36ea101d261c963d
Martin Vidner <martin@vidner.net>
2015-05-21 17:47:44 +0200

    WIP

--- lib/dbus/message_queue.rb
+++ lib/dbus/message_queue.rb
@@ -32,7 +32,7 @@ module DBus
       unless non_block
         # we can block
         while message.nil?
-          r, d, d = IO.select([@socket])
+          r, d, d = IO.select([@socket], [], [], @timeout)
           if r and r[0] == @socket
             buffer_from_socket_nonblock
             message = message_from_buffer_nonblock
